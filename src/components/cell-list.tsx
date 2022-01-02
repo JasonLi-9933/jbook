41185078531b6ef69667ba1d5ea26ee822115a39
Jason Li <haotian-li@outlook.com>
2022-01-02 14:48:53 -0800

    ready to deloy to npm

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,19 +1,24 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import { useActions } from "../hooks/use-actions";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
-import { Fragment, useRef } from "react";
-import './cell-list.css';
+import { Fragment, useRef, useEffect } from "react";
+import "./cell-list.css";
+
 const CellList: React.FC = () => {
   const listRef = useRef<HTMLDivElement>(null);
-
-   const cells = useTypedSelector(({ cells: { order, data } }) => {
+  const { fetchCells, saveCells } = useActions();
+  const cells = useTypedSelector(({ cells: { order, data } }) => {
     return order.map((id) => data[id]);
   });
+  useEffect(() => {
+    fetchCells();
+  }, []);
 
   const renderedCells = cells.map((cell, i) => (
     <Fragment key={cell.id}>
       <CellListItem cell={cell} />
-      <AddCell previousCellID={cell.id} isLastOne={i === cells.length-1} />
+      <AddCell previousCellID={cell.id} isLastOne={i === cells.length - 1} />
     </Fragment>
   ));
 
--- src/state/action-creators/cellsActionCreators.ts
+++ src/state/action-creators/cellsActionCreators.ts
@@ -1,4 +1,6 @@
 import { ActionType } from "../action-types";
+import { Action } from "../actions";
+import axios from "axios";
 import {
   UpdateCellAction,
   DeleteCellAction,
@@ -6,7 +8,9 @@ import {
   InsertCellAfterAction,
   Direction,
 } from "../actions";
-import { CellTypes } from "../cell";
+import { CellTypes, Cell } from "../cell";
+import { Dispatch } from "react";
+import { RootState } from "../reducers";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -45,5 +49,43 @@ export const insertCellAfter = (
       id,
       type,
     },
+  };
+};
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.FETCH_CELLS,
+    });
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get("/cells");
+      dispatch({
+        type: ActionType.FETCH_CELLS_COMPLETE,
+        payload: data,
+      });
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.FETCH_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+    const cells = order.map((id) => data[id]);
+    try {
+      await axios.post("/cells", { cells });
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.SAVE_CELLS_ERROR,
+        payload: err.message
+      });
+    }
   };
 };
